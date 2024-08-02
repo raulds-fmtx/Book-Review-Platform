8b8115eb65dd131e31bf315382df088691d72315
raulds-fmtx <rdsflomo03@gmail.com>
2024-08-01 21:59:25 -0500

    Revert to previous commit

--- server/server.js
+++ server/server.js
@@ -1,42 +1,21 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
-// const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-async function startServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({ req, authMiddleware }),
-  });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  await server.start();
-  server.applyMiddleware({ app });
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-
-  // app.use(routes);
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
-  db.once("open", () => {
-    app.listen(PORT, () =>
-      console.log(`🌍 Now listening on localhost:${PORT}`)
-    );
-  });
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-startServer();
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
