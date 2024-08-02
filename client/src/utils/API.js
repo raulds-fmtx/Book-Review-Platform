730907d33793211d197b6f9ccd3e2cc89e0431a0
raulds-fmtx <rdsflomo03@gmail.com>
2024-08-01 20:50:52 -0500

    API Key Added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -53,5 +53,7 @@ export const deleteBook = (bookId, token) => {
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  return fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyBOZpIHo-PKvqz7q30OLsjSWlux74iPMM8`
+  );
 };
