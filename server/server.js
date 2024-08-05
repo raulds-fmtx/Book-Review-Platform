265e406adf4be5207fd17f370e33ffaf08c9a407
raulds-fmtx <rdsflomo03@gmail.com>
2024-08-05 14:39:55 -0500

    Final backend refactoring changes made. Note that the routes and controllers directories can be deleted.

--- server/server.js
+++ server/server.js
@@ -25,13 +25,13 @@ async function startServer() {
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
   }
 
   // app.use(routes);
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 
   db.once("open", () => {
     app.listen(PORT, () =>
