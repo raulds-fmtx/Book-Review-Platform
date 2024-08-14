c6ba478a4246852aa72cf235c227717317ac1c85
raulds-fmtx <rdsflomo03@gmail.com>
2024-08-13 21:48:07 -0500

    More changes

--- server/server.js
+++ server/server.js
@@ -31,10 +31,10 @@ async function startServer() {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../client/public")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      res.sendFile(path.join(__dirname, "../client/public/index.html"));
     });
   }
 
